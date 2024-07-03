1412a1a7819f722fbe83e4991a4a55537ea301ca
NikCan <nikitagaponov@gmail.com>
2024-07-03 15:07:44 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const NLPCloudClient = require('nlpcloud');
+
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+const YOUR_API_KEY = 'da6f75b991066bdecba775d1ac85070e4cfbe68a';
+const YOUR_MODEL_NAME = 'finetuned-llama-3-70b';
+
+const client = new NLPCloudClient({model:YOUR_MODEL_NAME,token:YOUR_API_KEY,gpu:true})
+
+app.post('/nlp', async (req, res) => {
+    const { input, context, history } = req.body;
+  client.chatbot({ input, context, history })
+    .then(function (response) {
+    res.json(response.data);
+  })
+  .catch(function (err) {
+    res.status(500).json({ error: err.response.data.detail })
+  });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
