fea74a190767776d7ad2bd24a32d26d59732bcfa
NikCan <nikitagaponov@gmail.com>
2024-07-03 15:51:55 +0300

    cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors')
 const NLPCloudClient = require('nlpcloud');
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 const port = 3000;
